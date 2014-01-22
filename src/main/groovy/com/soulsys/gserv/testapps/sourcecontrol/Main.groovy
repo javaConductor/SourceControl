9a4043a64a7b4b06b296868311a38f6bb7b4e8a4
leecollins <javaConductor@yahoo.com>
2014-01-22 01:52:52 -0600

    interim

---
+++ src/main/groovy/com/soulsys/gserv/testapps/sourcecontrol/Main.groovy
@@ -0,0 +1,16 @@
+package com.soulsys.gserv.testapps.sourcecontrol
+
+/**
+ * Created by leecollins on 1/22/14.
+ */
+class Main {
+
+
+    static void main(String[] args){
+
+        def svc = new SourceControlRestSvc()
+
+    }
+
+
+}
